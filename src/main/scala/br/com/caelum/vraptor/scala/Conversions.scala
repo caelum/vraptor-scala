cc4f5ce9fabda09c679a4aa0f099e681b20a7504
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2011-12-22 17:57:30 -0200

    adding Scala validator

--- src/main/scala/br/com/caelum/vraptor/scala/Conversions.scala
+++ src/main/scala/br/com/caelum/vraptor/scala/Conversions.scala
@@ -1,7 +1,10 @@
 package br.com.caelum.vraptor.scala
 
-import br.com.caelum.vraptor.Result
+import br.com.caelum.vraptor.{Validator, Result}
+
 
 object Conversions {
    implicit def result2scalaResult(result: Result) = new ScalaResult(result)
+
+   implicit def validator2scalaValidator(validator:Validator) = validator.asInstanceOf[ScalaValidator]
 }
---
+++ src/main/scala/br/com/caelum/vraptor/scala/ScalaValidator.scala
@@ -0,0 +1,30 @@
+package br.com.caelum.vraptor.scala
+
+import br.com.caelum.vraptor.{Result, Validator}
+import br.com.caelum.vraptor.ioc.Component
+import br.com.caelum.vraptor.core.Localization
+import br.com.caelum.vraptor.proxy.Proxifier
+import br.com.caelum.vraptor.validator._
+import br.com.caelum.vraptor.view.ValidationViewsFactory
+
+@Component
+class ScalaValidator(result:Result, factory:ValidationViewsFactory, outjector:Outjector, proxifier:Proxifier,
+                     beanValidators:List[BeanValidator], localization:Localization)
+        extends DefaultValidator(result, factory, outjector, proxifier, beanValidators, localization) {
+
+  def check(test: Boolean, i18nKey: String = "", category: String = ""): Validator = {
+    checking(new Validations {
+      that(test, category, i18nKey)
+    })
+    this
+  }
+
+  def onError(f: Result => Unit) {
+    if (hasErrors) {
+      result.include("errors", getErrors)
+      outjector.outjectRequestMap()
+      f(result)
+      throw new ValidationException(getErrors)
+    }
+  }
+}
