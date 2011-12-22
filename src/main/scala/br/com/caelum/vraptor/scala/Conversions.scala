79b45d7f90619a9ff04a1e52b206d71e918310dd
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2011-12-22 18:08:19 -0200

    converting to ScalaValidator

--- src/main/scala/br/com/caelum/vraptor/scala/Conversions.scala
+++ src/main/scala/br/com/caelum/vraptor/scala/Conversions.scala
@@ -6,5 +6,7 @@ import br.com.caelum.vraptor.{Validator, Result}
 object Conversions {
    implicit def result2scalaResult(result: Result) = new ScalaResult(result)
 
-   implicit def validator2scalaValidator(validator:Validator) = validator.asInstanceOf[ScalaValidator]
+   implicit def validator2scalaValidator(validator:Validator) = {
+     validator.asInstanceOf[ScalaValidator]
+   }
 }
